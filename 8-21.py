7de50ce7dd9f0eed01c3b0ccf6c4cec0539360a2
pranav-pathare <pranavpathare.pp@gmail.com>
2026-08-21 10:31:16 -0400

    Added Variables & Datatypes

--- 8-21.py
+++ 8-21.py
@@ -1,3 +1,23 @@
-print("Hello")
+#Variables
+import array as arr
 
-print("Hello Deepak")
+a = 10
+b = 10.00
+c = "Hello World"
+d = True
+e = {1, 2, 3, 4, 5}
+f = ["apple", "banana", "cherry", 1, 2, 3, 4, 5]
+g = ("apple", "banana", "cherry", 1, 2, 3, 4, 5)
+h = arr.array('i', [1, 2, 3, 4, 5])
+
+
+#Data Types
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))  
