ac7f54bbb2649f81f602ad86fa5e711e26d37645
pranav-pathare <pranavpathare.pp@gmail.com>
2026-08-25 19:09:08 +0530

    updated

--- 8-24.py
+++ 8-24.py
@@ -1,8 +1,7 @@
+#Printing Table of any number using for loop
 a=int(input("Enter a number: "))
 #a = input("Enter a number: ")
 print("")
 for i in range(1,11):
     print(f"{a*i}") 
 print("")
-
-#Printing Table of any number using for loop
